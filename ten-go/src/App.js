c620cb9b4843b15b72d52ac8fee0c5387a6cb4fc
Zhuohang Liu <zhuohang@usc.edu>
2021-05-24 21:27:22 -0700

    Initial necessities

--- ten-go/src/App.js
+++ ten-go/src/App.js
@@ -1,7 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1JYV0MEDqfIT_aDkCdpr4ItrJp3OyBV0",
+  authDomain: "ten-go-5e4ef.firebaseapp.com",
+  projectId: "ten-go-5e4ef",
+  storageBucket: "ten-go-5e4ef.appspot.com",
+  messagingSenderId: "159468804228",
+  appId: "1:159468804228:web:a8524afc6a934e85844ad1",
+  measurementId: "G-07E1TPLW3K"
+};
+
 function App() {
+  var app;
+  if (!firebase.apps.length) {
+    app = firebase.initializeApp(firebaseConfig);
+  } else {
+    app = firebase.app(); // if already initialized, use that one
+  }
+
+  var db = firebase.firestore();
+  
+  console.log("Test");
+  db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  }) 
+  .then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+    console.error("Error adding document: ", error);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
--- ten-go/src/index.js
+++ ten-go/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <p>Hello world!</p>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -27,3 +28,17 @@ reportWebVitals();
 
 // <!-- Initialize Firebase -->
 // <script src="/__/firebase/init.js"></script>
+
+// Config object
+/*
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1JYV0MEDqfIT_aDkCdpr4ItrJp3OyBV0",
+  authDomain: "ten-go-5e4ef.firebaseapp.com",
+  projectId: "ten-go-5e4ef",
+  storageBucket: "ten-go-5e4ef.appspot.com",
+  messagingSenderId: "159468804228",
+  appId: "1:159468804228:web:a8524afc6a934e85844ad1",
+  measurementId: "G-07E1TPLW3K"
+};
+*/
